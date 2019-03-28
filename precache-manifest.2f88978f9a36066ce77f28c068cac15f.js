82d8124a3eca3178da8be931ddf81455bdd9a46b
liyong003 <liyong003@chinasofti.com>
2019-03-28 17:26:50 +0800

    react-router+react-redux项目

---
+++ precache-manifest.2f88978f9a36066ce77f28c068cac15f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "8c188fe9657c6047ab04",
+    "url": "./static/js/main.8c188fe9.chunk.js"
+  },
+  {
+    "revision": "e0da5b53d13904b5d059",
+    "url": "./static/js/1.e0da5b53.chunk.js"
+  },
+  {
+    "revision": "8c188fe9657c6047ab04",
+    "url": "./static/css/main.3dd59fea.chunk.css"
+  },
+  {
+    "revision": "13cf5ed001e51d380251793d7c98d618",
+    "url": "./index.html"
+  }
+];
